a9717d390c8ecf65f9fadb3dbf9b56ab81b24c7f
Partho Bhoumik <57154993+parthoBhoumk@users.noreply.github.com>
2023-12-04 23:42:49 -0700

    Update CNN2layer.py

--- CNN+FNN/FashionMNIST/CNN2layer.py
+++ CNN+FNN/FashionMNIST/CNN2layer.py
@@ -3,26 +3,27 @@ import torch.nn as nn
 def CNNmodel():
     model = nn.Sequential(
         # First convolution layer
-        nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1),
-        nn.ReLU(),
-        nn.BatchNorm2d(32),
-        nn.MaxPool2d(kernel_size=2, stride=2),
-
-        # Second convolution layer
-        nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
+        nn.Conv2d(in_channels=1, out_channels=64, kernel_size=5, padding=1),
         nn.ReLU(),
         nn.BatchNorm2d(64),
         nn.MaxPool2d(kernel_size=2, stride=2),
-        nn.Dropout2d(0.25),
+        nn.Dropout2d(0.3),
+
+        # Second convolution layer
+        nn.Conv2d(in_channels=64, out_channels=128, kernel_size=5),
+        nn.ReLU(),
+        nn.BatchNorm2d(128),
+        nn.MaxPool2d(kernel_size=2, stride=2),
+        nn.Dropout2d(0.3),
 
         # Flatten
         nn.Flatten(),
 
         # Fully connected layers
-        nn.Linear(64 * 6 * 6, 128),
+        nn.Linear(128* 4 * 4, 128),
         nn.ReLU(),
         nn.BatchNorm1d(128),
-        nn.Dropout(0.15),
+        nn.Dropout(0.3),
 
         nn.Linear(128, 10)
     )
