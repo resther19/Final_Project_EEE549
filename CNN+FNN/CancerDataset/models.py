ebc8454d841b107f8a5d270d3bf50fe76606a198
Partho Bhoumik <57154993+parthoBhoumk@users.noreply.github.com>
2023-12-04 23:22:08 -0700

    Update models.py

--- CNN+FNN/CancerDataset/models.py
+++ CNN+FNN/CancerDataset/models.py
@@ -2,6 +2,7 @@
 # %%
 import torch.nn as nn
 import torch
+
 class FNN1(nn.Module):
     def __init__(self): 
         super(FNN1, self).__init__()
@@ -12,16 +13,18 @@ class FNN1(nn.Module):
         x = self.fc1(x)
         return x
 
+
+
 class FNN2(nn.Module):
     def __init__(self, dropout_prob=0.2): 
         super(FNN2, self).__init__()
 
-        self.fc1 = nn.Linear(30, 64)
+        self.fc1 = nn.Linear(30, 120)
 
-        self.bn1 = nn.BatchNorm1d(64)
+        self.bn1 = nn.BatchNorm1d(120)
         self.dropout1 = nn.Dropout(dropout_prob)
 
-        self.fc2 = nn.Linear(64, 2)
+        self.fc2 = nn.Linear(120, 2)
 
     def forward(self, x):
         x = self.fc1(x)
