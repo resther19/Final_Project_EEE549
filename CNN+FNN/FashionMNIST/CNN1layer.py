ae9279d7747549f2df5cba6f3d260d8b2d833182
Partho Bhoumik <57154993+parthoBhoumk@users.noreply.github.com>
2023-12-04 23:42:18 -0700

    Update CNN1layer.py

--- CNN+FNN/FashionMNIST/CNN1layer.py
+++ CNN+FNN/FashionMNIST/CNN1layer.py
@@ -3,16 +3,18 @@ import torch.nn as nn
 def CNNmodel():
     model = nn.Sequential(
         # First convolution layer
-        nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1),
+        nn.Conv2d(in_channels=1, out_channels=128, kernel_size=5, padding=1),
         nn.ReLU(),
-        nn.BatchNorm2d(32),
+        nn.BatchNorm2d(128),
+
         nn.MaxPool2d(kernel_size=2, stride=2),
 
         # Flatten
         nn.Flatten(),
 
         # Fully connected layers
-        nn.Linear(32 * 14 * 14, 128),  
+        nn.Linear(128 * 13 * 13, 128),  
+
         nn.ReLU(),
         nn.BatchNorm1d(128),
         nn.Dropout(0.15),
@@ -20,4 +22,5 @@ def CNNmodel():
         nn.Linear(128, 10)  
     )
     return model
+
 
